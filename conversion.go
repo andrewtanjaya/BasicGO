01cabe312cac9054e9550ad7d0ad28e1a7b87422
andrewtanjaya <andrewtanjaya21@gmail.com>
2021-07-16 18:54:02 +0700

    Data type conversion

---
+++ conversion.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	angka32 := 500000
+	angka64 := int64(angka32)
+	angka8 := int8(angka32)
+
+	fmt.Println(angka64)
+	fmt.Println(angka32)
+
+	// overflow
+	fmt.Println(angka8)
+
+	name := "Andrew"
+	a := name[0]
+	fmt.Println(string(a))
+
+}
