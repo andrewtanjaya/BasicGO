1fd2fa6063f0583700ccd2e3c15d1a1b24f6705f
andrewtanjaya <andrewtanjaya21@gmail.com>
2021-07-16 19:07:43 +0700

    Math Operator

---
+++ math-operation.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	result := 10+10
+	fmt.Println(result)
+	fmt.Println(10*10)
+
+	// augmented assignment
+	// a = a + 10
+	a := 10
+	a += 10
+	fmt.Println(a)
+
+	// unary operator
+	// a = a+1
+	a ++
+	fmt.Println(a)
+}
