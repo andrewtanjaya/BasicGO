0549b0a9c36be6358af075391fc11b909e9df07d
andrewtanjaya <andrewtanjaya21@gmail.com>
2021-07-20 19:41:58 +0700

    map

--- map.go
+++ map.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	orang := map[string]string{
+		"nama" : "Andrew",
+		"alamat" : "abc",
+	}
+
+	orang["telp"] = "01321321"
+	fmt.Println(orang["telp"])
+	fmt.Println(len(orang))
+	delete(orang, "telp")
+	fmt.Println(orang["telp"])
 }
